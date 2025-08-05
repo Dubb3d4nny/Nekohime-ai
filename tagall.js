d3ecbb0d45fac4e8815cbdba5ac652aea0b04283
Dubb3d4nny <danielace007.exe@gmail.com>
2025-08-05 08:18:46 +0100

    Create tagall.js

---
+++ tagall.js
@@ -0,0 +1,14 @@
+// commands/tagall.js
+
+const tagAllMembers = async (sock, message, groupMetadata) => {
+    const participants = groupMetadata.participants;
+    const mentions = participants.map((p) => p.id);
+    const tagMessage = participants.map((p, i) => `${i + 1}. @${p.id.split('@')[0]}`).join('\n');
+
+    await sock.sendMessage(message.key.remoteJid, {
+        text: `📢 *Group Mention by NekoHime:*\n\n${tagMessage}`,
+        mentions
+    });
+};
+
+export default tagAllMembers;
